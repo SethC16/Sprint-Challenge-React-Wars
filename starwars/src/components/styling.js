5908a602e127d67951171d66fc2bcb561296f3bc
Seth Cox <Sethcox16@yahoo.com>
2020-01-24 13:15:15 -0500

    Created CharacterList.js, setup the function w/ useState and useEffect, made axios.get, passed the data to the map and named the elements.  Created the CharacterCards.js, created my const CharacterCard and input the div, h2, and p passing in the props.  Created the Styling.js, added styling for the CharacterCard and also for the SecDiv wrapping around that.

---
+++ starwars/src/components/styling.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const NewDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background: black;
+    color: white;
+    width: 200px;
+    max-height: 300px;
+    margin-bottom: 2%;
+    padding: 1%;
+`;
+
+export const SecDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    
+
+
+
+
+`
